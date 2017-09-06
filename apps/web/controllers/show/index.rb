99cb2167641a9c0a22752e8f0f4ad17e8e13e695
arturcuringa <arturmcuringa@gmail.com>
2017-09-06 17:38:12 -0400

    Add episode list in show template, by exporting in controller

--- apps/web/controllers/show/index.rb
+++ apps/web/controllers/show/index.rb
@@ -1,9 +1,23 @@
+#require 'feedjira'
+require 'rss'
+require 'open-uri'
+
 module Web::Controllers::Show
   class Index
     include Web::Action
-     expose :pod
+	expose :podcast
+	expose :episodes
     def call(params)	
-	pod = PodcastsRepository.get_by_id(params[:id])
+	@podcast = PodcastRepository.new.find(params[:id])
+	@episodes = list_episodes()
+    end
+private 
+    def list_episodes()
+	url = PodcastRepository.new.find(params[:id]).url
+	open(url) do |rss|
+	 feed = RSS::Parser.parse(rss)
+	  return feed.items
+ 	end
     end
   end
 end
