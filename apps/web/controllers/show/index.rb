a812321969754f4162e59e45d5c0965665f28b02
João Pedro Holanda <jpholanda.prf@gmail.com>
2017-09-06 21:29:12 -0400

    show things

--- apps/web/controllers/show/index.rb
+++ apps/web/controllers/show/index.rb
@@ -5,19 +5,22 @@ require 'open-uri'
 module Web::Controllers::Show
   class Index
     include Web::Action
-	expose :podcast
-	expose :episodes
+
+    expose :podcast
+    expose :episodes
+
     def call(params)	
-	@podcast = PodcastRepository.new.find(params[:id])
-	@episodes = list_episodes()
+    	@podcast = PodcastRepository.new.find(params[:id])
+    	@episodes = list_episodes
     end
-private 
-    def list_episodes()
-	url = PodcastRepository.new.find(params[:id]).url
-	open(url) do |rss|
-	 feed = RSS::Parser.parse(rss)
-	  return feed.items
- 	end
+    
+    private 
+    def list_episodes
+      url = PodcastRepository.new.find(params[:id]).url
+      open(url) do |rss|
+        feed = RSS::Parser.parse(rss)
+        feed.items
+      end
     end
   end
 end
