b6b34e4865adcfca0182d3f9b775b25e018f6ae8
arturcuringa <arturmcuringa@gmail.com>
2017-09-30 09:47:47 -0400

    Fix user model, add encrypted password

--- lib/getcast/entities/user.rb
+++ lib/getcast/entities/user.rb
@@ -1,2 +1,17 @@
 class User < Hanami::Entity
+	attributes do
+		attribute :id,  Types::Int
+		attribute :first_name, Types::String
+		attribute :last_name, Types::String
+		attribute :email, Types::String
+		attribute :password, Type::String
+		attribute :encrypted_password, Type::String
+		attribute :terms_of_service, Types::Bool
+		attribute :created_at,         Types::Time
+		attribute :updated_at,         Types::Time
+	end
+
+	def password
+		Password.new(super)
+	end
 end
