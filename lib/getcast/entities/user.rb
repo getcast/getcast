9dfb876c057d61d68d1296025c268feb0c91488e
arturcuringa <arturmcuringa@gmail.com>
2017-09-30 11:48:48 -0400

    Add test for encrypted password

--- lib/getcast/entities/user.rb
+++ lib/getcast/entities/user.rb
@@ -1,17 +1,18 @@
 class User < Hanami::Entity
+	EMAIL_FORMAT = /\@/
 	attributes do
 		attribute :id,  Types::Int
 		attribute :first_name, Types::String
 		attribute :last_name, Types::String
-		attribute :email, Types::String
-		attribute :password, Type::String
-		attribute :encrypted_password, Type::String
+		attribute :email, Types::String.constrained(format: EMAIL_FORMAT)
+		attribute :password, Types::String
+		attribute :encrypted_password, Types::String
 		attribute :terms_of_service, Types::Bool
 		attribute :created_at,         Types::Time
 		attribute :updated_at,         Types::Time
 	end
 
 	def password
-		Password.new(super)
+		Encrypter.new(super)
 	end
 end
--- spec/getcast/entities/user_spec.rb
+++ spec/getcast/entities/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  # place your tests here
+	before do
+		@user = User.new(first_name: "Steven", last_name: "Universe",
+				 email: "stevenuniverse@getcast.com", password: Encrypter.generate("gemsRuby"),
+				terms_of_service: true )
+	end
+	it 'password encryption works' do
+		assert_instance_of Encrypter, @user.password 
+		assert_equal(@user.password,"gemsRuby")		
+	end
+
 end
