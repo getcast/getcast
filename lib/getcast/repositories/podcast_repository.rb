e4ff4cbc101f00c5ed914c4ac3a2ac12c701fce2
carlosemv <vieiramecarlos@gmail.com>
2017-10-08 15:56:28 -0400

    change last_updated to store when feed was actually updated when possible, refactor code into get_feed in podcast_repository

--- lib/getcast/repositories/podcast_repository.rb
+++ lib/getcast/repositories/podcast_repository.rb
@@ -13,9 +13,58 @@ class PodcastRepository < Hanami::Repository
     podcasts.count > 0
   end
 
-  def update_cache(id, xml)
+  def update_cache(id, xml, feed)
     update(id, cache: xml)
-    update(id, last_updated: DateTime.now)
+    if feed.last_built
+      begin
+        time = Time.httpdate(feed.last_built.chomp('+0000')+'GMT')
+      rescue ArgumentError # last_built not a valid string
+        entry = update(id, last_updated: DateTime.now)
+      else
+        entry = update(id, last_updated: time)
+      end
+    else
+      entry = update(id, last_updated: DateTime.now)
+    end
+    entry
+  end
+
+  def connection(url, headers, request_options)
+    Faraday.new(url: url, headers: headers, request: request_options) do |conn|
+      conn.use FaradayMiddleware::FollowRedirects, limit: Feedjira.follow_redirect_limit
+      conn.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_feed(id)
+    feed = nil
+    podcast = find(id)
+    if !podcast
+      return feed
+    end
+
+    begin
+      if !podcast.cache
+        xml = Feedjira::Feed.connection(podcast.url).get.body
+        feed = Feedjira::Feed.parse_with(Feedjira::Parser::ITunesRSS, xml)
+        update_cache(id, xml, feed)
+      else
+        headers = {user_agent: Feedjira.user_agent, 
+          if_modified_since: podcast.last_updated.httpdate}
+        request_options = {timeout: Feedjira.request_timeout}
+        xml = connection(podcast.url, headers, request_options).get.body
+        if not xml.empty?
+          feed = Feedjira::Feed.parse_with(Feedjira::Parser::ITunesRSS, xml)
+          update_cache(id, xml, feed)
+        else
+          feed = Feedjira::Feed.parse_with(Feedjira::Parser::ITunesRSS, podcast.cache)
+        end
+      end
+    rescue Feedjira::FetchFailure, Faraday::Error, Zlib::DataError
+      raise
+    end
+
+    feed
   end
 
   @@ATTRS = [:url, :title, :description].freeze
