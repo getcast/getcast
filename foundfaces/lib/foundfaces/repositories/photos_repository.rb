81062fb394b5e7d2f36f05a8623306caaf4f023e
Artur Curinga <arturmcuringa@gmail.com>
2017-11-21 19:46:07 -0300

    Add photos model on hanami and main prototype

---
+++ foundfaces/lib/foundfaces/repositories/photos_repository.rb
@@ -0,0 +1,2 @@
+class PhotosRepository < Hanami::Repository
+end
--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
 require_relative 'framework/pooler'
+require_relative 'foundfaces/lib/repositories/'
+
+src = {}
+src[:tag] = "found faces"
 
 pooler = Pooler(source: {})
+pooler.pool
