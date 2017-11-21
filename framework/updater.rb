deacebd8d6edf349160d625171eba79d10014c45
Carlos Vieira <carlosv@MacBook-Pro-de-Carlos.local>
2017-11-21 19:46:59 -0300

    implement updater for foundfaces

--- framework/updater.rb
+++ framework/updater.rb
@@ -1,9 +1,32 @@
 class Updater
-	def verify(source)
-		raise "not implemented"
+	def initialize()
+		@flickr = Flickr.new('flickr.yml')
+	end
+
+	def verify(source, last_updated)
+		photos = flickr.photos.search(:text => source, 
+			:min_upload_date => last_updated)
+		if photos.size > 0
+			true
+		else
+			false
+		end
 	end
 
 	def update(source)
-		raise "not implemented"
+		photos = flickr.photos.search(:text => search_text, :per_page => 500)
+		new_pages = photos.pages
+
+		new_photos = Set.new
+		(1..new_pages).to_a.each do |page|
+			photos = flickr.photos.search(:text => search_text, 
+				:per_page => 500, :page => page)
+
+			photos.each do |photo|
+				new_photos << photo.url(:medium)
+		  	end
+		end
+
+		new_photos
 	end
 end
