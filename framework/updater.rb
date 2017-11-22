70952d11025ef87b2722260ff36882e563a7bd54
João Pedro Holanda <jpholanda.prf@gmail.com>
2017-11-22 11:42:54 -0300

    Revert "implement updater for foundfaces"
    This reverts commit deacebd8d6edf349160d625171eba79d10014c45.

--- framework/updater.rb
+++ framework/updater.rb
@@ -1,32 +1,9 @@
 class Updater
-	def initialize()
-		@flickr = Flickr.new('flickr.yml')
-	end
-
-	def verify(source, last_updated)
-		photos = flickr.photos.search(:text => source, 
-			:min_upload_date => last_updated)
-		if photos.size > 0
-			true
-		else
-			false
-		end
+	def verify(source)
+		raise "not implemented"
 	end
 
 	def update(source)
-		photos = flickr.photos.search(:text => search_text, :per_page => 500)
-		new_pages = photos.pages
-
-		new_photos = Set.new
-		(1..new_pages).to_a.each do |page|
-			photos = flickr.photos.search(:text => search_text, 
-				:per_page => 500, :page => page)
-
-			photos.each do |photo|
-				new_photos << photo.url(:medium)
-		  	end
-		end
-
-		new_photos
+		raise "not implemented"
 	end
 end
