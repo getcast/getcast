53609158451c7338ddf0d79dd930b5045f2d09fc
João Pedro Holanda <jpholanda.prf@gmail.com>
2017-11-27 10:53:43 -0300

    pooler with threads

--- framework/pooler.rb
+++ framework/pooler.rb
@@ -7,13 +7,22 @@ class Pooler
 
 	def pool
 		loop do
+			threads = []
 			@extractors.each_pair do |source, extractor|
-				if extractor.verify(source)
-					new_data = extractor.extract(source)
-					@repositories[source].each do |repository|
-						repository.batch_update(new_data)
+				t = Thread.new do
+					if extractor.verify(source)
+						new_data = extractor.extract(source)
+						@repositories[source].each do |repository|
+							repository.batch_update(new_data)
+						end
 					end
 				end
+				t.run
+				threads << t
+			end
+
+			threads.each do |t|
+				t.join
 			end
 
 			sleep @time
